f3f6899e196db03b20732c33ba730d706269d3f7
Durig, Burak <burak.durig@nissan-usa.com>
2024-12-27 16:16:57 -0600

    adding Live Data

--- app/src/main/java/com/mastercoding/viewmodelapp/MainActivity.java
+++ app/src/main/java/com/mastercoding/viewmodelapp/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.mastercoding.viewmodelapp.databinding.ActivityMainBinding;
@@ -22,21 +23,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main);
 
         viewModel = new ViewModelProvider(this)
                 .get(MyViewModel.class);
 
-        mainBinding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModel.increaseCounter();
-                mainBinding.textView2.setText(""+viewModel.getCounter());
-            }
-        });
-        // without the empty string the application will fail
-        mainBinding.textView2.setText(""+viewModel.getCounter());
+        mainBinding.setMyviewmodel(viewModel);
 
 
+
+        //We can get rid of the below on click listners since we are using databinding.
+//        mainBinding.button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                viewModel.increaseCounter();
+//                //mainBinding.textView2.setText(""+viewModel.getCounter());
+//                // if the above is not used or commented out the counter will never display
+//                // to resolve this we need live data
+//            }
+//        });
+        // without the empty string, "", the application will fail because the viewmodel is int
+        // and you can't set a string type to int
+        //mainBinding.textView2.setText(""+viewModel.getCounter());
+        // the above also needs to be commented out or otherwise you get failures.
+
+        // Observing the LiveData
+        viewModel.getCounter().observe(this,
+                new Observer<Integer>() {
+                    @Override
+                    public void onChanged(Integer counter) {
+                        // update the UI when the LiveData changes
+                        mainBinding.textView2.setText(""+counter);
+                    }
+                });
     }
 }
--- app/src/main/java/com/mastercoding/viewmodelapp/MyViewModel.java
+++ app/src/main/java/com/mastercoding/viewmodelapp/MyViewModel.java
@@ -2,17 +2,36 @@ package com.mastercoding.viewmodelapp;
 
 import android.view.View;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
-    int counter = 0;
+    //int counter = 0;
+    // above is without live data, below is with live data
+    private MutableLiveData<Integer> counter = new MutableLiveData<>();
 
-    public void increaseCounter(){
-        counter++;
+    // this will hep to redude chance of memory leaks.
+
+    // since we got rid of the onclick listeners in the main we are going to add the View view as an input.
+    // this would require a creation of a data variable within the xml.
+    public void increaseCounter(View view){
+        //counter++;
+        // above is part of non live data
+        int currentValue = counter.getValue() != null ? counter.getValue():0;
+        // if the above counter.getValue() does not equal null, then set it to 0.
+        // the above is to prevent non-null exemptions. or the above will initialize
+        // if not already initialized
+
+        //the getvalue and setValue are part of the livedata.
+
+        counter.setValue(currentValue+1);
     }
 
-    public int getCounter(){
+    public LiveData<Integer> getCounter(){
         return counter;
+
+        // replace the int return type to LiveData<Integer>
     }
 
 }
